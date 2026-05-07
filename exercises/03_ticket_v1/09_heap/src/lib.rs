1453f92d67145718c5e8bb32a6657ad7d1208417
theharshpat <harsh.patel.magic105@gmail.com>
2026-05-07 13:19:57 +0530

    ticket v1 exercises completed

--- exercises/03_ticket_v1/09_heap/src/lib.rs
+++ exercises/03_ticket_v1/09_heap/src/lib.rs
@@ -13,7 +13,7 @@ mod tests {
 
     #[test]
     fn string_size() {
-        assert_eq!(size_of::<String>(), todo!());
+        assert_eq!(size_of::<String>(), 24);
     }
 
     #[test]
@@ -23,6 +23,51 @@ mod tests {
         // but, in general, the memory layout of structs is a more complex topic.
         // If you're curious, check out the "Type layout" section of The Rust Reference
         // https://doc.rust-lang.org/reference/type-layout.html for more information.
-        assert_eq!(size_of::<Ticket>(), todo!());
+        assert_eq!(size_of::<Ticket>(), 24 * 3);
+    }
+
+    #[test]
+    fn checking_string_ref_size_and_value() {
+        let mut s = String::with_capacity(5);
+
+        println!("=== BEFORE PUSH ===");
+
+        // Address of the String struct itself (stack location)
+        println!("address of s (String struct): {:p}", &s);
+
+        // Heap buffer pointer stored INSIDE String
+        println!("s.as_ptr() (heap buffer):     {:p}", s.as_ptr());
+
+        println!("len: {}, cap: {}", s.len(), s.capacity());
+
+        s.push_str("Hey");
+
+        println!("\n=== AFTER PUSH ===");
+
+        println!("address of s (String struct): {:p}", &s);
+        println!("s.as_ptr() (heap buffer):     {:p}", s.as_ptr());
+
+        println!("len: {}, cap: {}", s.len(), s.capacity());
+
+        let r: &String = &s;
+
+        println!("\n=== REFERENCE ===");
+
+        // r points TO s
+        println!("r value (address it stores):  {:p}", r);
+
+        // address of the reference variable itself
+        println!("address of r variable:        {:p}", &r);
+
+        // heap pointer through the reference
+        println!("r.as_ptr() (heap buffer):     {:p}", r.as_ptr());
+
+        println!("\n=== CHECKS ===");
+
+        // r stores address of s
+        assert_eq!(r as *const String, &s as *const String);
+
+        // both see same heap buffer
+        assert_eq!(r.as_ptr(), s.as_ptr());
     }
 }
