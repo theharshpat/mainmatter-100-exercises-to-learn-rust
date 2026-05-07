96ad9d2a7e2de9157bcddf760c76dd67a2019234
theharshpat <harsh.patel.magic105@gmail.com>
2026-05-07 16:50:36 +0530

    04 traits wip - exercises 00 to 10 (last is no 14)

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -13,6 +13,140 @@
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
 
+
+// My initial approach - passes tests but not optimal
+//
+// Mental model at this stage:
+// - `Power<T>` means "a family of traits parameterized by T"
+// - Therefore:
+//     Power<u16>
+//     Power<u32>
+//     Power<&u32>
+//   are effectively different trait contracts.
+//
+// - That's why these are all legal simultaneously:
+//     impl Power<u16> for u32
+//     impl Power<u32> for u32
+//     impl Power<&u32> for u32
+//
+// - Generic parameter `T` is the INPUT type.
+// - We use a generic parameter because we want MULTIPLE implementations
+//   for different exponent/input types.
+//
+// - At this stage I used:
+//     fn power(self, ...)
+//   which consumes ownership of self.
+//
+// - That technically works because `u32: Copy`,
+//   but consuming ownership is unnecessarily restrictive.
+//
+// - After reading more about trait design + associated types,
+//   I realized borrowing is more idiomatic here because exponentiation
+//   only needs read access.
+//
+// - Important syntax magic:
+//     2_u32.power(3)
+//   still works even if method takes `&self`.
+//
+//   Rust auto-borrows method receivers:
+//     2_u32.power(3)
+//   becomes approximately:
+//     (&2_u32).power(3)
+//
+// - General API design heuristic:
+//     Prefer &self unless ownership is actually required.
+
+
+// Better approach after revisiting the concepts.
+
+trait Power<T> {
+
+    // `T` is a generic parameter because we want MANY possible input types.
+    //
+    // These are all different implementations:
+    //     Power<u16>
+    //     Power<u32>
+    //     Power<&u32>
+    //
+    // Generic parameters are chosen from OUTSIDE the trait impl
+    // based on the caller/input types.
+    //
+    // Changed:
+    //     self -> &self
+    //
+    // Reason:
+    // - exponentiation does not logically require ownership
+    // - method only needs temporary read access
+    // - borrowing is less restrictive and more reusable
+    // - more idiomatic for non-consuming operations
+    //
+    // Mental model:
+    //     self  => "I take ownership"
+    //     &self => "I only need read access"
+    //
+    // Returning `Self` here means:
+    // - output type is always the implementing type
+    // - simpler than using an associated Output type
+    //
+    // This is okay for this exercise because:
+    //     u32 ^ anything -> u32
+    //
+    // If we later needed:
+    //     some_type.power(x) -> different_type
+    // then we'd introduce:
+    //     type Output;
+    //
+    // Similar to std::ops::Add:
+    //     trait Add<Rhs> {
+    //         type Output;
+    //     }
+    //
+    // Associated types are used when the output type must be
+    // UNIQUELY determined by the implementation.
+    fn power(&self, n: T) -> Self;
+}
+
+impl Power<u16> for u32 {
+
+    // `u16` exponent version.
+    //
+    // Compiler selects this impl because exponent type is `u16`.
+    //
+    // `.pow()` expects u32 exponent,
+    // so convert u16 -> u32 using `.into()`.
+    fn power(&self, n: u16) -> Self {
+        self.pow(n.into())
+    }
+}
+
+impl Power<u32> for u32 {
+
+    // Direct u32 exponent implementation.
+    fn power(&self, n: u32) -> Self {
+        self.pow(n)
+    }
+}
+
+impl Power<&u32> for u32 {
+
+    // Borrowed exponent version.
+    //
+    // `n` is `&u32`, but `.pow()` needs owned `u32`,
+    // so dereference using `*n`.
+    //
+    // Could also delegate:
+    //
+    //     self.power(*n)
+    //
+    // which would reuse the `Power<u32>` impl.
+    //
+    // That delegation style is often more idiomatic because it
+    // centralizes the actual logic in one implementation.
+    fn power(&self, n: &u32) -> Self {
+        self.pow(*n)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Power;
