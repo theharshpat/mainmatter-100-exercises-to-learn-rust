713e5a0b3e729c78723ed551a56c5784b44d0058
theharshpat <harsh.patel.magic105@gmail.com>
2026-05-11 14:42:51 +0530

    06 ticket management - 00 to 04

--- exercises/06_ticket_management/01_arrays/src/lib.rs
+++ exercises/06_ticket_management/01_arrays/src/lib.rs
@@ -1,7 +1,7 @@
 // TODO: Flesh out the `WeekTemperatures` struct and its method implementations to pass the tests.
 
 pub struct WeekTemperatures {
-    // TODO
+    week_day_temperature: [Option<i32>; 7]
 }
 
 pub enum Weekday {
@@ -16,15 +16,17 @@ pub enum Weekday {
 
 impl WeekTemperatures {
     pub fn new() -> Self {
-        todo!()
+        Self{
+            week_day_temperature: [None; 7]
+        }
     }
 
     pub fn get_temperature(&self, day: Weekday) -> Option<i32> {
-        todo!()
+        self.week_day_temperature[day as usize]
     }
 
     pub fn set_temperature(&mut self, day: Weekday, temperature: i32) {
-        todo!()
+        self.week_day_temperature[day as usize] = Some(temperature);
     }
 }
 
--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,12 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut memo = vec![0, 1];
+
+    for i in 2..=n {
+        memo.push(memo[(i-2) as usize] + memo[(i-1) as usize]);
+    }
+    memo[n as usize]
 }
 
 #[cfg(test)]
