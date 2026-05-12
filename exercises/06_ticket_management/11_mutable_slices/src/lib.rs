547ae46d7ba449275b38d3d19c3de74dd8a86a6f
theharshpat <harsh.patel.magic105@gmail.com>
2026-05-12 14:35:14 +0530

    06 ticket management - 10, 11

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,6 +1,10 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
+pub fn squared(slice: &mut [i32]) {
+    slice.iter_mut().for_each(|n| *n = *n * *n);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
